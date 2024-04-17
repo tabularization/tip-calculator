c15d376567a3e590d4c172573bb5b109f0ffe61c
tabularization <127825421+tabularization@users.noreply.github.com>
2024-04-17 02:49:49 -0400

    Implement basic features

--- src/App.js
+++ src/App.js
@@ -1,56 +1,113 @@
 import { useState } from "react";
 
-
 export default function App() {
   const [cost, setCost] = useState(0);
   const [personalTip, setPersonalTip] = useState(0);
   const [friendTip, setFriendTip] = useState(0);
 
+  function handleSetCost(cost) {
+    setCost(cost);
+  }
+
+  function handleSetPersonalTip(tip) {
+    setPersonalTip(tip);
+  }
+
+  function handleSetFriendTip(tip) {
+    setFriendTip(tip);
+  }
+
+  function handleReset() {
+    setCost(0);
+    setPersonalTip(0);
+    setFriendTip(0);
+  }
+
   return (
     <div className="App">
       <h1>Tip Calculator</h1>
-      <Bill cost={cost}></Bill>
-      <PersonalReview personalTip={personalTip}></PersonalReview>
-      <FriendReview friendTip={friendTip}></FriendReview>
+      <Bill cost={cost} onSetCost={handleSetCost}/>
+      <PersonalReview personalTip={personalTip} onSetPersonalTip={handleSetPersonalTip}/>
+      <FriendReview friendTip={friendTip} onSetFriendTip={handleSetFriendTip}/>
+      <Results cost={cost} personalTip={personalTip} friendTip={friendTip}/>
+      <Button onReset={handleReset}>Reset!</Button>
     </div>
   );
 }
 
-function Bill({ cost, children }) {
+function Bill({ cost, onSetCost }) {
   return (
     <div>
-      <label for="billCost" className="text">How much was the bill?</label>
-      <input id="billCost" type="text" placeholder={cost}></input>
+      <label for="billCost" className="text">
+        How much was the bill?
+      </label>
+      <input id="billCost" type="text" placeholder={cost} onChange={(e)=>onSetCost(e.target.value)}></input>
     </div>
   );
 }
 
-function PersonalReview({ personalTip }) {
+function PersonalReview({ personalTip, onSetPersonalTip}) {
   return (
     <div>
       <label for="personalReview" className="text">
         How did your like the service?
       </label>
-      <input
+      <select
         id="personalReview"
         type="text"
         placeholder={`${personalTip}%`}
-      ></input>
+        onChange={(e)=>onSetPersonalTip(e.target.value)}
+      >
+        <option value="0">Dissastisfied (0%)</option>
+        <option value="15" selected>
+          Sastified (15%)
+        </option>
+        <option value="20">Amazing (20%)</option>
+        <option value="30">Extraordinary (30%)</option>
+      </select>
     </div>
   );
 }
 
-function FriendReview({ friendTip }) {
+function FriendReview({ friendTip, onSetFriendTip }) {
   return (
     <div>
       <label for="friendReview" className="text">
         How did your friend like the service?
       </label>
-      <input
+      <select
         id="friendReview"
         type="text"
         placeholder={`${friendTip}%`}
-      ></input>
+        onChange={(e) => onSetFriendTip(e.target.value)}
+      >
+        <option value="0" selected>
+          Dissastisfied (0%)
+        </option>
+        <option value="15">Sastified (15%)</option>
+        <option value="20">Amazing (20%)</option>
+        <option value="30">Extraordinary (30%)</option>
+      </select>
     </div>
+  );
+}
+
+function Results({ cost, personalTip, friendTip}) {
+  const tipPercent = (parseFloat(personalTip) + parseFloat(friendTip)) / 2
+  return (
+    <div>
+      <h2>
+        Your total bill is ${cost * (1 + tipPercent / 100)}. You
+        tipped ${cost * (tipPercent / 100)} ({tipPercent}%).
+      </h2>
+    </div>
+  );
+}
+
+function Button({ onReset, children }) {
+  return (
+    <>
+      <button onClick={onReset}>{children}</button>
+    </>
   );
 }
